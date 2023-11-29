6ed2452421961f645d75fe70cf50b8d5cf5f4d4b
thinguyen3131 <thi.nguyen@prismtechinc.io>
2023-11-29 14:09:42 +0700

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-
 export default function HomePage() {
   return (
     <Container>
+      <MainSlider />
+      <MainSlider />
       <MainSlider />
     </Container>
   );
