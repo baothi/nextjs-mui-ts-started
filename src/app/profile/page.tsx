112648a636fded6a4ca0dac6771fa31090467425
baothi <baothi246@gmail.com>
2023-12-10 21:01:17 +0700

    add footer

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ProfilePage = () => {
+  // console.log("ProfilePage is available on your application :", process.env.NEXT_PUBLIC_BACKEND_URL);
   return (
     <div>Profile page</div>
   )
