c8750be83f4d5a55555986d74ebc08a4777a0cbb
baothi <baothi246@gmail.com>
2023-12-10 21:51:02 +0700

    check api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 import AppFooter from "@/components/footer/app.footer";
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
-export default function HomePage() {
+// import { sendRequestJS } from "@/utils/old.api";
+import { sendRequest } from "@/utils/api";
+export default async function HomePage() {
+  // const res = await fetch("http://localhost:8000/api/v1/tracks/top",{
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     category: "CHILL",
+  //     limit: 10
+  //   })
+  // });
+  // console.log("check response server returned : ", await res.json());
+  const res = await sendRequest<IBackendRes<ITrackTop>>({
+    url: "http://localhost:8000/api/v1/tracks/top",
+    method: "POST",
+    body: { category: "CHILL", limit: 2},
+    
+  });
+  console.log("check response server returned : ", res.data);
   return (
     <Container>
       <MainSlider />
