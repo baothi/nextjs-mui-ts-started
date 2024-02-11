e3a07ff1b978ead13d4e4bb79f3bb40ad45e8fc2
baothi <baothi246@gmail.com>
2024-02-11 17:05:12 +0700

    add middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import AppFooter from '@/components/footer/app.footer';
 import NextAuthProvider from '@/lib/next.auth.provider';
 import { ToastProvider } from '@/utils/toast';
 import { TrackContextProvider } from '@/lib/track.wrapper';
+import NProgressWrapper from '@/lib/nprogress.wrapper';
 
 
 
@@ -13,13 +14,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <NextAuthProvider>
-            <ToastProvider>
-              <TrackContextProvider>
-                {children}
-              </TrackContextProvider>
-            </ToastProvider>
-          </NextAuthProvider>
+          <NProgressWrapper>
+            <NextAuthProvider>
+              <ToastProvider>
+                <TrackContextProvider>
+                  {children}
+                </TrackContextProvider>
+              </ToastProvider>
+            </NextAuthProvider>
+          </NProgressWrapper>
         </ThemeRegistry>
       </body>
     </html>
